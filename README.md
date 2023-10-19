cf9f8e86ed6639f7e357ca00f9d3967133998fad
javedmughal <javedmughal609@gmail.com>
2023-10-19 15:17:49 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# video_calling_using_zegocloud_in_flutter
+ This is practice app for video calling using zegocloud
---
+++ android/app/src/main/kotlin/com/example/video_calling_using_zegocloud_in_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.video_calling_using_zegocloud_in_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
